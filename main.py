6ef4e53f67c1c6acca5e2c0210fc8aeb306a385e
AuxyPotato <lucyschonewille@gmail.com>
2025-06-23 23:21:04 +0200

    split up code into functions
    added proper prompting functionality

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt()
+    )
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    print_response(response, prompt_tokens, response_tokens)
 
-print(response.text)
-print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-print("Response tokens: ", response.usage_metadata.candidates_token_count)
+
+def prompt():
+    if len(sys.argv) != 2:
+        print("Error: No prompt provided or too many arguments")
+        sys.exit(1)
+    else:
+        return sys.argv[1]
+
+
+def print_response(response, prompt_tokens, response_tokens):
+    print(response.text)
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
+
+
+
+if __name__ == "__main__":
+    main()
