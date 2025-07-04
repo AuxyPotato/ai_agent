15f8bbb072db9363490e4563f8392990e1f7c3ba
AuxyPotato <lucyschonewille@gmail.com>
2025-07-05 00:26:26 +0200

    added function to execute python code im sure nothing will go wrong there

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,3 +1,29 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path):
+    absolute_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not absolute_file_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.exists(absolute_file_path):
+        return f'Error: File "{file_path}" not found.'
+    elif not absolute_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    else:
+        try:
+            output = subprocess.run(['python3', absolute_file_path],
+                                    capture_output=True, cwd=os.path.abspath(working_directory), timeout=30)
+
+            stdout = output.stdout.decode("utf-8")
+            stderr = output.stderr.decode("utf-8")
+            result = f'STDOUT: {stdout}\nSTDERR: {stderr}'
+            if output.returncode != 0:
+                result += f'\nProcess exited with code {output.returncode}'
+            if len(stdout) == 0 and len(stderr) == 0 and output.returncode == 0:
+                return 'No output produced.'
+
+            return result
+
+        except (subprocess.TimeoutExpired, Exception) as e:
+            return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -3,10 +3,14 @@ import os
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def main():
-    pass
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
 
 
 
