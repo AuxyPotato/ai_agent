2db2d15c3c309d8abf4a2d50273f3b26ab1799af
AuxyPotato <lucyschonewille@gmail.com>
2025-07-04 22:56:41 +0200

    added file writing function

--- tests.py
+++ tests.py
@@ -1,13 +1,12 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
 import os
 
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+
+
 def main():
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat"))
-
-
+    pass
 
 
 
